af6cfa23965cb577726d6df23b4291e9e3c88eab
ddavison <daniel.jj.davison@gmail.com>
2013-09-15 22:48:35 -0400

    Adding validationFunctions and more stuff.

--- src/main/java/com/company/seleniumframework/AutomationTest.java
+++ src/main/java/com/company/seleniumframework/AutomationTest.java
@@ -2,6 +2,7 @@ package com.company.seleniumframework;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * the base test that includes all Selenium 2 functionality that you will need
@@ -24,7 +26,10 @@ import org.openqa.selenium.safari.SafariDriver;
  */
 public class AutomationTest {
 
-    WebDriver driver;
+    public WebDriver driver;
+    
+    // max seconds before failing a script.
+    private final int MAX_ATTEMPTS = 5;
     
     public Map<String, By> props = new HashMap<String, By>();
     
@@ -52,6 +57,7 @@ public class AutomationTest {
         	properties.load(getClass().getResourceAsStream(getClass().getSimpleName().concat(".properties")));
         	
         	for (String key : properties.stringPropertyNames())
+        		// css is arbitrary here.. USE IT! It rocks!
         		props.put(key, By.cssSelector(properties.getProperty(key)));
         } 
         catch (Exception x) {
@@ -67,12 +73,38 @@ public class AutomationTest {
     }
     
     /**
+     * Private method that acts as an arbiter of implicit timeouts of sorts.. sort of like a Wait For Ajax method.
+     */
+    private WebElement waitForElement(By by) {
+    	int attempts = 0;
+    	int size = driver.findElements(by).size();
+    	
+    	while (size == 0) {
+    		size = driver.findElements(by).size();
+    		if (attempts == MAX_ATTEMPTS) fail(String.format("Could not find %s after %d seconds",
+    		                                                 by.toString(),
+    		                                                 MAX_ATTEMPTS));
+    		attempts++;
+    		try {
+    			Thread.sleep(1000); // sleep for 1 second.
+    		} catch (Exception x) {
+    			fail("Failed due to an exception during Thread.sleep!");
+    			x.printStackTrace();
+    		}
+    	}
+    	
+    	if (size > 0) System.err.println("WARN: There are more than 1 " + by.toString() + " 's!");
+    	
+    	return driver.findElement(by);
+    }
+    
+    /**
      * Click an element.
      * @param by The element to click.
      * @return
      */
     public AutomationTest click(By by) {
-        driver.findElement(by).click();
+    	waitForElement(by).click();
         return this;
     }
     
@@ -84,27 +116,40 @@ public class AutomationTest {
      */
     public AutomationTest setText(By by, String text) {
     	System.out.println(by.toString());
-        driver.findElement(by).sendKeys(text);
+        waitForElement(by).sendKeys(text);
         return this;
     }
     
     /**
      * Checks if the element is checked or not.
      * @param by
-     * @return
+     * @return <i>this method is not meant to be used fluently.</i><br><br>
+     * Returns <code>true</code> if the element is checked. and <code>false</code> if it's not.
      */
     public boolean isChecked(By by) {
-        return driver.findElement(by).isSelected();
+        return waitForElement(by).isSelected();
+    }
+    
+    /**
+     * Checks if the element is present or not.<br>
+     * @param by
+     * @return <i>this method is not meant to be used fluently.</i><br><br.
+     * Returns <code>true</code> if the element is present. and <code>false</code> if it's not.
+     */
+    public boolean isPresent(By by) {
+    	if (driver.findElements(by).size() > 0) return true;
+    	return false;
     }
     
     /**
      * Get the text of an element.
+     * <blockquote>This is a consolidated method that works on input's, as select boxes, and fetches the value rather than the innerHTMl.</blockquote>
      * @param by
      * @return
      */
     public String getText(By by) {
     	String text = null;
-    	WebElement e = driver.findElement(by);
+    	WebElement e = waitForElement(by);
     	
     	if (e.getTagName().equalsIgnoreCase("input") || e.getTagName().equalsIgnoreCase("select"))
     		text = e.getAttribute("value");
@@ -121,7 +166,7 @@ public class AutomationTest {
      */
     public AutomationTest check(By by) {
 	    if (!isChecked(by)) {
-	    	driver.findElement(by).click();
+	    	waitForElement(by).click();
 	    	assertTrue(by.toString() + " did not check!", isChecked(by));
 	    }
 	    return this;
@@ -134,13 +179,61 @@ public class AutomationTest {
      */
     public AutomationTest uncheck(By by) {
     	if (isChecked(by)) {
-    		driver.findElement(by).click();
+    		waitForElement(by).click();
     		assertFalse(by.toString() + " did not uncheck!", isChecked(by));
     	}
     	return this;
     }
     
+    /**
+     * Selects an option from a dropdown ({@literal <select> tag}) based on the text displayed.
+     * @param by
+     * @param text The text that is displaying.
+     * @see #selectOptionByValue(By, String)
+     * @return
+     */
+    public AutomationTest selectOptionByText(By by, String text) {
+    	Select box = new Select(waitForElement(by));
+    	box.selectByVisibleText(text);
+    	return this;
+    }
+    
+    /**
+     * Selects an option from a dropdown ({@literal <select> tag}) based on the value.
+     * @param by
+     * @param value The <code>value</code> attribute of the option.
+     * @see #selectOptionByText(By, String)
+     * @return
+     */
+    public AutomationTest selectOptionByValue(By by, String value) {
+    	Select box = new Select(waitForElement(by));
+    	box.selectByValue(value);
+    	return this;
+    }
+    
     /* Validation Functions for Testing */
+    
+    /**
+     * Validates that an element is present.
+     * @param by
+     * @return
+     */
+    public AutomationTest validatePresent(By by) {
+    	waitForElement(by);
+    	assertTrue("Element " + by.toString() + " does not exist!",
+    			isPresent(by));
+    	return this;
+    }
+    
+    /**
+     * Validates that an element is not present.
+     * @param by
+     * @return
+     */
+    public AutomationTest validateNotPresent(By by) {
+    	assertFalse("Element " + by.toString() + " exists!", isPresent(by));
+    	return this;
+    }
     
     /**
      * Validate that the text of an element is correct.
@@ -150,6 +243,38 @@ public class AutomationTest {
      */
     public AutomationTest validateText(By by, String text) {
     	assertTrue("Text does not match!", getText(by).equals(text));
+    	return this;
+    }
+    
+    /**
+     * Validate that a checkbox or a radio button is checked.
+     * @param by
+     * @return
+     */
+    public AutomationTest validateChecked(By by) {
+    	assertTrue(by.toString() + " is not checked!", isChecked(by));
+    	return this;
+    }
+    
+    /**
+     * Validate that a checkbox or a radio button is unchecked.
+     * @param by
+     * @return
+     */
+    public AutomationTest validateUnchecked(By by) {
+    	assertFalse(by.toString() + " is not unchecked!", isChecked(by));
+    	return this;
+    }
+    
+    /* ================================ */
+    
+    /**
+     * Navigates the browser back one page.
+     * Same as <code>driver.navigate().back()</navigate>
+     * @return
+     */
+    public AutomationTest goBack() {
+    	driver.navigate().back();
     	return this;
     }
     
