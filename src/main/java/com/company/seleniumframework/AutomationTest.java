6bc59c82f7057ee3d93a8002a984fab9f7c9f215
ddavison <daniel.davison@proquest.com>
2013-09-30 18:05:55 -0400

    adding more stuff

--- src/main/java/com/company/seleniumframework/AutomationTest.java
+++ src/main/java/com/company/seleniumframework/AutomationTest.java
@@ -6,10 +6,15 @@ import static org.junit.Assert.fail;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Properties;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.junit.After;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,6 +36,8 @@ public class AutomationTest {
     // max seconds before failing a script.
     private final int MAX_ATTEMPTS = 5;
     
+    private int attempts = 0;
+    
     public Map<String, By> props = new HashMap<String, By>();
     
     /**
@@ -38,6 +45,9 @@ public class AutomationTest {
      */
     private String baseUrl;
     private Config configuration;
+    
+    private Pattern p;
+    private Matcher m;
     
     public AutomationTest() {
         configuration = getClass().getAnnotation(Config.class);
@@ -54,14 +64,14 @@ public class AutomationTest {
         // load the properties.
         Properties properties = new Properties();
         try {
-        	properties.load(getClass().getResourceAsStream(getClass().getSimpleName().concat(".properties")));
-        	
-        	for (String key : properties.stringPropertyNames())
-        		// css is arbitrary here.. USE IT! It rocks!
-        		props.put(key, By.cssSelector(properties.getProperty(key)));
+            properties.load(getClass().getResourceAsStream(getClass().getSimpleName().concat(".properties")));
+            
+            for (String key : properties.stringPropertyNames())
+                // css is arbitrary here.. USE IT! It rocks!
+                props.put(key, By.cssSelector(properties.getProperty(key)));
         } 
         catch (Exception x) {
-        	x.printStackTrace();
+            x.printStackTrace();
         }
         
         driver.navigate().to(baseUrl);
@@ -69,33 +79,33 @@ public class AutomationTest {
     
     @After
     public void teardown() {
-    	driver.quit();
+        driver.quit();
     }
     
     /**
      * Private method that acts as an arbiter of implicit timeouts of sorts.. sort of like a Wait For Ajax method.
      */
     private WebElement waitForElement(By by) {
-    	int attempts = 0;
-    	int size = driver.findElements(by).size();
-    	
-    	while (size == 0) {
-    		size = driver.findElements(by).size();
-    		if (attempts == MAX_ATTEMPTS) fail(String.format("Could not find %s after %d seconds",
-    		                                                 by.toString(),
-    		                                                 MAX_ATTEMPTS));
-    		attempts++;
-    		try {
-    			Thread.sleep(1000); // sleep for 1 second.
-    		} catch (Exception x) {
-    			fail("Failed due to an exception during Thread.sleep!");
-    			x.printStackTrace();
-    		}
-    	}
-    	
-    	if (size > 0) System.err.println("WARN: There are more than 1 " + by.toString() + " 's!");
-    	
-    	return driver.findElement(by);
+        int attempts = 0;
+        int size = driver.findElements(by).size();
+        
+        while (size == 0) {
+            size = driver.findElements(by).size();
+            if (attempts == MAX_ATTEMPTS) fail(String.format("Could not find %s after %d seconds",
+                                                             by.toString(),
+                                                             MAX_ATTEMPTS));
+            attempts++;
+            try {
+                Thread.sleep(1000); // sleep for 1 second.
+            } catch (Exception x) {
+                fail("Failed due to an exception during Thread.sleep!");
+                x.printStackTrace();
+            }
+        }
+        
+        if (size > 0) System.err.println("WARN: There are more than 1 " + by.toString() + " 's!");
+        
+        return driver.findElement(by);
     }
     
     /**
@@ -104,19 +114,21 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest click(By by) {
-    	waitForElement(by).click();
+        waitForElement(by).click();
         return this;
     }
     
     /**
-     * Sets the text of a particular element, with some text.
+     * Clears the text from a text field, and sets it.
      * @param by The element to set the text of.
      * @param text The text that the element will have.
      * @return
      */
     public AutomationTest setText(By by, String text) {
-    	System.out.println(by.toString());
-        waitForElement(by).sendKeys(text);
+        System.out.println(by.toString());
+        WebElement element = waitForElement(by);
+        element.clear();
+        element.sendKeys(text);
         return this;
     }
     
@@ -137,8 +149,8 @@ public class AutomationTest {
      * Returns <code>true</code> if the element is present. and <code>false</code> if it's not.
      */
     public boolean isPresent(By by) {
-    	if (driver.findElements(by).size() > 0) return true;
-    	return false;
+        if (driver.findElements(by).size() > 0) return true;
+        return false;
     }
     
     /**
@@ -148,15 +160,15 @@ public class AutomationTest {
      * @return
      */
     public String getText(By by) {
-    	String text = null;
-    	WebElement e = waitForElement(by);
-    	
-    	if (e.getTagName().equalsIgnoreCase("input") || e.getTagName().equalsIgnoreCase("select"))
-    		text = e.getAttribute("value");
-    	else
-    		text = e.getText();
-    	
-    	return text;
+        String text = null;
+        WebElement e = waitForElement(by);
+        
+        if (e.getTagName().equalsIgnoreCase("input") || e.getTagName().equalsIgnoreCase("select"))
+            text = e.getAttribute("value");
+        else
+            text = e.getText();
+        
+        return text;
     }
     
     /**
@@ -165,11 +177,11 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest check(By by) {
-	    if (!isChecked(by)) {
-	    	waitForElement(by).click();
-	    	assertTrue(by.toString() + " did not check!", isChecked(by));
-	    }
-	    return this;
+        if (!isChecked(by)) {
+            waitForElement(by).click();
+            assertTrue(by.toString() + " did not check!", isChecked(by));
+        }
+        return this;
     }
     
     /**
@@ -178,11 +190,11 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest uncheck(By by) {
-    	if (isChecked(by)) {
-    		waitForElement(by).click();
-    		assertFalse(by.toString() + " did not uncheck!", isChecked(by));
-    	}
-    	return this;
+        if (isChecked(by)) {
+            waitForElement(by).click();
+            assertFalse(by.toString() + " did not uncheck!", isChecked(by));
+        }
+        return this;
     }
     
     /**
@@ -193,9 +205,9 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest selectOptionByText(By by, String text) {
-    	Select box = new Select(waitForElement(by));
-    	box.selectByVisibleText(text);
-    	return this;
+        Select box = new Select(waitForElement(by));
+        box.selectByVisibleText(text);
+        return this;
     }
     
     /**
@@ -206,10 +218,170 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest selectOptionByValue(By by, String value) {
-    	Select box = new Select(waitForElement(by));
-    	box.selectByValue(value);
-    	return this;
+        Select box = new Select(waitForElement(by));
+        box.selectByValue(value);
+        return this;
     }
+    
+    /* Window / Frame Switching */
+    
+    /**
+     * Waits for a window to appear, then switches to it.
+     * @param regex Regex enabled. Url of the window, or title.
+     * @return
+     */
+    public AutomationTest waitForWindow(String regex) {
+        Set<String> windows = driver.getWindowHandles();
+
+        for (String window : windows) {
+            try {
+                driver.switchTo().window(window);
+
+                p = Pattern.compile(regex);
+                m = p.matcher(driver.getCurrentUrl());
+
+                if (m.find()) {
+                    attempts = 0;
+                    return switchToWindow(regex);
+                }
+                else {
+                    // try for title
+                    m = p.matcher(driver.getTitle());
+                    
+                    if (m.find()) {
+                        attempts = 0;
+                        return switchToWindow(regex);
+                    }
+                    else {
+                        if (attempts <= MAX_ATTEMPTS) {
+                            attempts++;
+                            
+                            try {Thread.sleep(1000);}catch(Exception x) { x.printStackTrace(); }
+
+                            return waitForWindow(regex);
+                        } else {
+                            fail("Window with url|title: " + regex + " did not appear after " + MAX_ATTEMPTS + " tries. Exiting.");
+                        }
+                    }
+                }
+            } catch(NoSuchWindowException e) {
+                if (attempts <= MAX_ATTEMPTS) {
+                    attempts++;
+                    
+                    try {Thread.sleep(1);}catch(Exception x) { x.printStackTrace(); }
+
+                    return waitForWindow(regex);
+                } else {
+                    fail("Window with url|title: " + regex + " did not appear after " + MAX_ATTEMPTS + " tries. Exiting.");
+                }
+            }
+        }
+
+        // when we reach this point, that means no window exists with that title..
+        if (attempts == MAX_ATTEMPTS) {
+            fail("Window with title: " + regex + " did not appear after 5 tries. Exiting.");
+            return this;
+        } else {
+            System.out.println("#waitForWindow() : Window doesn't exist yet. [" + regex + "] Trying again. " + attempts + "/" + MAX_ATTEMPTS);
+            attempts++;
+            return waitForWindow(regex);
+        }
+    }
+    
+    /**
+     * Switch's to a window that is already in existance.
+     * @param regex Regex enabled. Url of the window, or title.
+     * @return
+     */
+    public AutomationTest switchToWindow(String regex) {
+        Set<String> windows = driver.getWindowHandles();
+        
+        for (String window : windows) {
+            driver.switchTo().window(window);
+            System.out.println(String.format("#switchToWindow() : title=%s ; url=%s",
+                    driver.getTitle(),
+                    driver.getCurrentUrl()));
+            
+            p = Pattern.compile(regex);
+            m = p.matcher(driver.getTitle());
+            
+            if (m.find()) return this;
+            else {
+                m = p.matcher(driver.getCurrentUrl());
+                
+                if (m.find()) return this;
+                else fail("Couldn't switch to window with title / url : " + regex);
+            }
+        }
+        
+        return this;
+    }
+    
+    /**
+     * Close an open window.
+     * <br>
+     * If you have opened only 1 external window, then when you call this method, the context will switch back to the window you were using before.<br>
+     * <br>
+     * If you had more than 2 windows displaying, then you will need to call {@link #switchToWindow(String)} to switch back context.
+     * @param regex The title of the window to close (regex enabled). You may specify <code>null</code> to close the active window. If you specify <code>null</code> then the context will switch back to the initial window.
+     * @return
+     */
+    public AutomationTest closeWindow(String regex) {
+        Set<String> windows = driver.getWindowHandles();
+        
+        for (String window : windows) {
+            try {
+                driver.switchTo().window(window);
+                
+                p = Pattern.compile(regex);
+                m = p.matcher(driver.getTitle());
+                
+                if (m.find()) {
+                    switchToWindow(regex); // switch to the window, then close it.
+                    driver.close();
+                    
+                    if (windows.size() == 2) // just default back to the first window.
+                        driver.switchTo().window(windows.iterator().next());
+                } else {
+                    m = p.matcher(driver.getCurrentUrl());
+                    if (m.find()) {
+                        switchToWindow(regex);
+                        driver.close();
+                        
+                        if (windows.size() == 2) driver.switchTo().window(windows.iterator().next());
+                    }
+                }
+                    
+            } catch(NoSuchWindowException e) {
+                fail("Cannot close a window that doesn't exist. ["+regex+"]");
+            }
+        }
+        return this;
+    }
+    
+    /**
+     * Closes the current active window.  Calling this method will return the context back to the initial window.
+     * @return
+     */
+    public AutomationTest closeWindow() {
+        return closeWindow(null);
+    }
+    
+    /**
+     * Switches to a frame or iframe.
+     * @param idOrName The id or name of the frame.
+     * @return
+     */
+    public AutomationTest switchToFrame(String idOrName) {
+        try {
+            driver.switchTo().frame(idOrName);
+        } catch (Exception x) {
+            fail("Couldn't switch to frame with id or name [" + idOrName + "]");
+        }
+        return this;
+    }
+    
+    /* ************************ */
     
     /* Validation Functions for Testing */
     
@@ -219,10 +391,10 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest validatePresent(By by) {
-    	waitForElement(by);
-    	assertTrue("Element " + by.toString() + " does not exist!",
-    			isPresent(by));
-    	return this;
+        waitForElement(by);
+        assertTrue("Element " + by.toString() + " does not exist!",
+                isPresent(by));
+        return this;
     }
     
     /**
@@ -231,8 +403,8 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest validateNotPresent(By by) {
-    	assertFalse("Element " + by.toString() + " exists!", isPresent(by));
-    	return this;
+        assertFalse("Element " + by.toString() + " exists!", isPresent(by));
+        return this;
     }
     
     /**
@@ -242,8 +414,10 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest validateText(By by, String text) {
-    	assertTrue("Text does not match!", getText(by).equals(text));
-    	return this;
+        String actual = getText(by);
+        
+        assertTrue(String.format("Text does not match! [expected: %s] [actual: %s]", text, actual), text.equals(actual));
+        return this;
     }
     
     /**
@@ -252,8 +426,8 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest validateChecked(By by) {
-    	assertTrue(by.toString() + " is not checked!", isChecked(by));
-    	return this;
+        assertTrue(by.toString() + " is not checked!", isChecked(by));
+        return this;
     }
     
     /**
@@ -262,8 +436,45 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest validateUnchecked(By by) {
-    	assertFalse(by.toString() + " is not unchecked!", isChecked(by));
-    	return this;
+        assertFalse(by.toString() + " is not unchecked!", isChecked(by));
+        return this;
+    }
+    
+    /**
+     * Validates an attribute of an element.<br><br>
+     * Example:<br>
+     * <blockquote>
+     * {@literal <input type="text" id="test" />}
+     * <br><br>
+     * <code>.validateAttribute(css("input#test"), "type", "text") // validates that the "type" attribute equals "test"</code>
+     * </blockquote>
+     * @param by The element
+     * @param attr The attribute you'd like to validate
+     * @param regex What the attribute <b>should</b> be.  (this method supports regex)
+     * @return
+     */
+    public AutomationTest validateAttribute(By by, String attr, String regex) {
+        String actual = null;
+        try {
+            actual = driver.findElement(by).getAttribute(attr);
+            if (actual.equals(regex)) return this; // test passes.
+        } catch (NoSuchElementException e) {
+            fail("No such element [" + by.toString() + "] exists.");
+        } catch (Exception x) {
+            fail("Cannot validate an attribute if an element doesn't have it!");
+        }
+        
+        p = Pattern.compile(regex);
+        m = p.matcher(actual);
+        
+        assertTrue(String.format("Attribute doesn't match! [Selector: %s] [Attribute: %s] [Desired value: %s] [Actual value: %s]", 
+                by.toString(),
+                attr,
+                regex,
+                actual
+                ), m.find());
+        
+        return this;
     }
     
     /* ================================ */
@@ -274,8 +485,8 @@ public class AutomationTest {
      * @return
      */
     public AutomationTest goBack() {
-    	driver.navigate().back();
-    	return this;
+        driver.navigate().back();
+        return this;
     }
     
 }
--- src/tests/java/com/company/seleniumframework/functional/SampleFunctionalTest.java
+++ src/tests/java/com/company/seleniumframework/functional/SampleFunctionalTest.java
@@ -1,6 +1,7 @@
 package com.company.seleniumframework.functional;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import com.company.seleniumframework.AutomationTest;
 import com.company.seleniumframework.Browser;
@@ -9,16 +10,55 @@ import com.company.seleniumframework.Config;
 /**
  * This is a sample test that can get you started.
  * <br><br>
- * This test shows how you can use the concept of "method chaining" to create successful, and independent tests. 
+ * This test shows how you can use the concept of "method chaining" to create successful, and independent tests, as well as the validations method that can get you started.
  * @author ddavison
  *
  */
 
-@Config(url = "http://google.com", browser = Browser.CHROME) // You are able to specify a "base url" for your test, from which you will test. You may leave `browser` blank.
+@Config(url = "http://sircapsalot.no-ip.biz:4444/tests/getting-started-with-selenium.htm", browser = Browser.FIREFOX) // You are able to specify a "base url" for your test, from which you will test. You may leave `browser` blank.
 public class SampleFunctionalTest extends AutomationTest {
-    @Test 
-    public void testGoogle() {
-        setText(props.get("google.txtSearch"), "test")
-        .click(props.get("google.btnSearch"));
+    
+    /**
+     * You are able to fire this test right up and see it in action.  Right click the test() method, and click "Run As... jUnit test".
+     * 
+     * The purpose of this is to show you how you can continue testing, just by taking the semi colon out, and continuing with your test.
+     */
+    @Test
+    public void test() {
+        
+        
+        // click / validateAttribute
+        click(props.get("click"))
+        .validateAttribute(props.get("click"), "class", "success") // validate that the class indeed added.
+        
+        // setText / validateText
+        .setText(By.id("setTextField"), "woot!")
+        .validateText(By.id("setTextField"), "woot!") // validates that it indeed set.
+        
+        // check / uncheck
+        .check(By.id("checkbox"))
+        .validateChecked(By.id("checkbox")) // validate that it checked
+
+        .check(props.get("radio.2")) // remember that props come from <class name>.properties, and are always CSS selectors. (why use anything else, honestly.)
+        .validateUnchecked(props.get("radio.1")) // since radio 1 was selected by default, check the second one, then validate that the first radio is no longer checked.
+        
+        // select from dropdowns.
+        .selectOptionByText(By.xpath("//select[@id='select']"), "Second") // just as a proof of concept that you can select on anything. But don't use xpath!!
+        .validateText(By.id("select"), "2") // validateText() will actually return the value="" attr of a dropdown, so that's why 2 works but "Second" will not.
+        
+        .selectOptionByValue(By.cssSelector("select#select"), "3")
+        .validateText(props.get("select"), "3")
+        
+        // frames
+        .switchToFrame("frame") // the id="frame"
+        .validatePresent(By.cssSelector("div#frame_content"))
+        
+        // windows
+        .switchToWindow("Getting Started with Selenium") // switch back to the test window.
+        .click(By.linkText("Open a new tab / window"))
+        .waitForWindow("Google") // waits for the url.  Can also be the you are expecting. :) (regex enabled too)
+        .setText(By.name("q"), "google!")
+        .closeWindow(); // we've closed google, and back on the getting started with selenium page.
+        
     }
 }
